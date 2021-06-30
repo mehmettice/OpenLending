4f6c544e970828c0413589dec48866f2ab20298a
mutlutice <88emrekaya88@gmail.com>
2021-06-30 10:19:31 -0500

    Based Project

--- src/test/java/com/OpenLending/runner/MainTest.java
+++ src/test/java/com/OpenLending/runner/MainTest.java
@@ -18,7 +18,7 @@ public class MainTest {
 
         @Before
         public void setUp() {
-            Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);git
             Driver.getDriver().manage().window().maximize();
             Driver.getDriver().get(ConfigurationReader.getProperty("url"));
 
